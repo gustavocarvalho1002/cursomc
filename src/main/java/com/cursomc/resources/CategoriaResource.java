729af8f50fa8c7a9075a89b9694c800685a5b8c4
gustavocarvalho1002 <gustavocarvalho1002@gmail.com>
2019-03-06 14:26:59 -0300

    Criação de excessão simples em Categoria

--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,17 @@
 package com.cursomc.resources;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.cursomc.domain.Categoria;
 import com.cursomc.services.CategoriaService;
+import com.cursomc.services.exceptions.ObjectNotFoundException;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -23,7 +24,15 @@ public class CategoriaResource {
 	//Método de busca por ID
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
-		return ResponseEntity.ok().body(obj);
+		//Categoria obj = service.buscar(id);
+		//return ResponseEntity.ok().body(obj);
+		
+		try {
+			Categoria obj = service.buscar(id);
+			return ResponseEntity.ok().body(obj);
+	    } catch (ObjectNotFoundException ex) {
+	        throw new ResponseStatusException(
+	          HttpStatus.NOT_FOUND, "Registro inexistente", ex);
+	    }
 	}
 }
