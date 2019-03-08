0e5923a2bb1b57abc2ce43ea108da3b707f4466d
gustavocarvalho1002 <gustavocarvalho1002@gmail.com>
2019-03-08 18:53:47 -0300

    EndPoint pedidos/id disponibilizado

--- src/main/java/com/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/cursomc/domain/PagamentoComBoleto.java
@@ -5,16 +5,18 @@ import java.util.Date;
 import javax.persistence.Entity;
 
 import com.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataVencimento;
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataPagamento;
 	
 	private PagamentoComBoleto() {
-		
 	}
 
 	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
